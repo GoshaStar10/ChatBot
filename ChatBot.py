002d42d7b476ff937e195a2653d291238498ef31
George <79626258+GoshaStar10@users.noreply.github.com>
2021-03-04 09:32:27 +0300

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,3 +1,20 @@
 class ChatBot():
-    keywords = {"greetings": ["HELLO", "HI"],
-                "questions": ["?", "HOW", "WHERE", "WHEN"]}
+    keywords = {"greetings": ["HELLO", "HI", "HEY"],
+                "questions": ["?", "HOW", "WHERE", "WHEN", "WHY", "WHAT", "WHO"]}
+
+     def __init__(self, type, name):
+            self.type = type
+            self.name = name
+            
+     def get_request(self, text):
+        sentences = list(text.split("."))
+        types = set()
+        for words in sentences:
+            words = words.split()
+            for word in words:
+                word = word.upper()
+                for current_type in ChatBot.keywords.keys():
+                    if word in ChatBot.keywords[current_type]:
+                        types.add(current_type)
+                       
+        return types
